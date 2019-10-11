feeb60f47005f2b646f749d429ef004fbe7b231f
mitsu.sasaki <mitsu32164620@gmail.com>
2019-10-11 07:29:55 +0000

    kaminari

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
   end
 
   def show
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to task_url
+    redirect_to tasks_url
   end
   
   
