ccd90f6db57f8919e56df0c0e355d7647f4b866c
mitsu.sasaki <mitsu32164620@gmail.com>
2019-10-19 20:15:40 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,15 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
     @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    
+   
   end
-
+  
+  
   def show
   end
 
@@ -13,13 +18,15 @@ class TasksController < ApplicationController
   end
 
   def create
+    
+    @task = current_user.tasks.build(task_params)
    
-    @task = Task.new(task_params)
-
+   
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -58,6 +65,13 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :title)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,18 @@
 module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+
+#    if @current_user.blank?
+#      @current_user = User.find_by(id:session[:user_id]) #userを探して@current_userにセットとする
+#    end
+#    @current_user
+
+#    if  # crerrnt_userに中身がある場合
+#      @current_user #何もしないのでそのまま返す
+#    else　　　#中身がなかった場合
+#      @current_user = User.find_by(id:session[:user_id]) #userを探して@current_userにセットとする
+#    end
+#    return @current_user
   end
 
   def logged_in?
